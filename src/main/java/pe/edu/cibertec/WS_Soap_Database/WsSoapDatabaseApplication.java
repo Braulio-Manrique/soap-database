7601e04d38aeeccf433c6f1ec49e76c30f3c379e
Braulio-Manrique <brauliocuenta12@gmail.com>
2024-10-13 09:07:22 -0500

    iniciar proyecto

---
+++ src/main/java/pe/edu/cibertec/WS_Soap_Database/WsSoapDatabaseApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WS_Soap_Database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSoapDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSoapDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/WS_Soap_Database/WsSoapDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WS_Soap_Database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSoapDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
